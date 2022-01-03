eaf15336b37dedf53c316919a177e3fd29a75f3b
Mahesh <mahesh.channapur@dhiomics.com>
2022-01-03 00:49:22 +0000

    Integrate

--- src/app/views/user-management/user/single-user/single-user.component.ts
+++ src/app/views/user-management/user/single-user/single-user.component.ts
@@ -1,6 +1,6 @@
 import { NgStyle } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { indexOf } from 'lodash';
 import { CitGlobalConstantService } from 'src/app/services/api-collection';
@@ -19,9 +19,10 @@ export class SingleUserComponent implements OnInit {
   breadCrumblocationsList: any = []
   loadingRouteConfig: boolean = false
 
+  result: any;
   //user-group
-  user_group_name : any;
-  name:any;
+  user_group_name: any;
+  name: any;
 
   constructor(
     private apiString: CitGlobalConstantService,
@@ -29,25 +30,21 @@ export class SingleUserComponent implements OnInit {
     private router: Router,
     private _formBuilder: FormBuilder) { }
   ngOnInit() {
-    // this.updateBreadCrumb()
+    this.updateBreadCrumb()
 
     this.firstFormGroup = this._formBuilder.group({
       first_name: ['', Validators.required],
       middle_name: [''],
       last_name: ['', Validators.required],
-      username: [ '',Validators.required],
-      email: ['', Validators.required],
-      phone_no: ['', Validators.required],
+      username: ['',Validators.required],
+      email: ['',[Validators.required,Validators.pattern("^[A-Za-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      phone_no: [null, [Validators.required, Validators.pattern("[0-9 ]{10}")]],
       address: ['', Validators.required],
     });
     this.secondFormGroup = this._formBuilder.group({
       user_group: ['', Validators.required]
     });
-
-    //testing
-    this.user_group()
-
-
+   
   }
   updateBreadCrumb() {
     this.breadCrumblocationsList = [
@@ -64,7 +61,7 @@ export class SingleUserComponent implements OnInit {
     ];
 
     window.scrollTo(0, 0);
-    console.log("breadCrumblocationsList", this.breadCrumblocationsList);
+    // console.log("breadCrumblocationsList", this.breadCrumblocationsList);
   }
   redirect(link: any) {
     console.log(link);
@@ -74,80 +71,81 @@ export class SingleUserComponent implements OnInit {
   }
 
   // Demographic
-  result:any;
-
-  valid_user(){
+  valid_user() {
     let body = {
-      username : this.firstFormGroup.value.username
-    } 
-    
-    this.apiMethod.get_request_Param(this.apiString.single_user.valid_user,body).subscribe(data=>{
-      
+      username: this.firstFormGroup.value.username
+    }
+    this.apiMethod.get_request_Param(this.apiString.single_user.valid_user, body).subscribe(data => {
       this.result = data
-      console.log('r = ',this.result.status)
+      console.log('r = ', this.result.status)
+      if (this.result.status == "Exist Username") {
+        this.apiMethod.popupMessage('error', 'UserName Exist!!')
+      }
+      else {
+        this.apiMethod.popupMessage('success', 'UserName Okk!!')
+      }
     })
-    if(this.result.status==="Exist Username"){
-      this.apiMethod.popupMessage('error', 'UserName Exist!!')
-    }
-    else{
-      this.apiMethod.popupMessage('success', 'Okk!!')
-    }
     
     // console.log(this.result.status)
   }
-  valid_email(){
+  valid_email() {
     let body = {
-      email : this.firstFormGroup.value.email
-    } 
-    
-    this.apiMethod.get_request_Param(this.apiString.single_user.valid_email,body).subscribe(data=>{
-      
+      email: this.firstFormGroup.value.email
+    }
+
+    this.apiMethod.get_request_Param(this.apiString.single_user.valid_email, body).subscribe(data => {
+
       this.result = data
       // console.log('r = ',this.result.status)
-
-    })
-    if(this.result.status==="Exist-Email"){
-      this.apiMethod.popupMessage('error', 'Email Already Exist!!')
-    }
-    else{
-      this.apiMethod.popupMessage('success', 'Okk!!')
-    }
-    console.log(this.firstFormGroup.value.email)
+      if (this.result.status === "Exist-Email") {
+        this.apiMethod.popupMessage('error', 'Email Already Exist!!')
+      }
+    }) 
+    // console.log(this.firstFormGroup.value.email)
   }
 
-  insert_values(){
-    let body = this.firstFormGroup.value;
+  user_group() {
 
-   this.apiMethod.post_request(this.apiString.single_user.insert_values, body).subscribe(data=>{
-    console.log('hiii',data)
-   })
-   this.apiMethod.popupMessage('success','Updated Successfully !!')
-  }
-
-  user_group(){
-    
-    this.apiMethod.get_request(this.apiString.group_user.user_access).subscribe(data=>{
+    this.apiMethod.get_request(this.apiString.group_user.user_access).subscribe(data => {
       // console.log('User_group : ',data)
       this.user_group_name = data;
       this.name = this.user_group_name.data
-      console.log(typeof (this.name))
     })
-    
+
   }
-  
-  selected_value(val : any){
-    let param = val
-      this.apiMethod.get_request_Param(this.apiString.group_user.group_description,param).subscribe(data=>{
-        console.log(data)
-      })
-    
-    console.log(param)
+
+  insert_values() {
+    let f_group = this.firstFormGroup.value
+    let s_group = this.secondFormGroup.value
+    let body = Object.assign({}, f_group, s_group);
+
+    this.apiMethod.post_request(this.apiString.single_user.insert_values, body).subscribe(data => {
+      this.result = data
+      console.log(this.result)
+      if(this.result.status_code == 204){
+        this.apiMethod.popupMessage('success', 'Updated Successfully !!')
+      }
+      else {
+        this.apiMethod.popupMessage('error','Not Updated !!')     
+      }
+    })
+   
+   
   }
+
+  // selected_value(val : any){
+  //   let param = val
+  //     this.apiMethod.get_request_Param(this.apiString.group_user.group_description,param).subscribe(data=>{
+  //       console.log(data)
+  //     })
+
+  //   console.log(param)
+  // }
 
   submit() {
     // console.log(this.firstFormGroup.value, this.secondFormGroup.value)
     this.insert_values()
   }
 
- 
+
 }
